8ef1988ae08222afa44606e44e2527af93b3a2c1
Dheeraj Arora <dheeraj.arora1997@yahoo.com>
2021-03-05 12:47:08 +0530

    Update custom-breadcrumb.php

--- custom-breadcrumb.php
+++ custom-breadcrumb.php
@@ -31,26 +31,26 @@ function get_the_slug($id = null)
 
 <div class="npf-breadcrumb">
     <?php
-    $url = home_url();
-    $grandparenturl = get_the_slug($grandparent);
-    $parenturl = get_the_slug($parent);
-    if ($root_parent = get_the_title($grandparent) !== $root_parent = get_the_title($child)){
-        echo "<a href='".$url."'><i class='fa fa-home'></i></a>&nbsp; »
-        <a href='".$url."/".$grandparenturl."'>". $grandparenturl ."</a> »
-        <a href='".$url."/".$grandparenturl."/".$parenturl."'>". $parenturl ."</a> »"
-        .get_the_slug($current);
-    }
-    else if($root_parent = get_the_title($parent) !== $root_parent = get_the_title($current)) {
-        echo "<a href='".$url."'><i class='fa fa-home'></i></a>&nbsp; »
-        <a href='".$url."/".$grandparenturl."/".$parenturl."'>". $parenturl ."</a> »"
-        .get_the_slug($current);
-    }
-    else if($root_parent = get_the_title($child) == $root_parent = get_the_title($child)) {
-        echo "<a href='".$url."'><i class='fa fa-home'></i></a>&nbsp; » "
-        .get_the_slug($current);;
-    }
-    else {
-        echo get_the_slug($child);
-    }
+        $url = home_url();
+        $grandparenturl = get_the_slug($grandparent);
+        $parenturl = get_the_slug($parent);
+        if ($root_parent = get_the_title($grandparent) !== $root_parent = get_the_title($child)){
+            echo "<a href='".$url."'><i class='fa fa-home'></i></a>&nbsp;»&nbsp;
+            <a href='".$url."/".$grandparenturl."'>". $grandparenturl ."</a>&nbsp;»&nbsp;
+            <a href='".$url."/".$grandparenturl."/".$parenturl."'>". $parenturl ."</a>&nbsp;»&nbsp;"
+            .get_the_slug($current);
+        }
+        else if($root_parent = get_the_title($parent) !== $root_parent = get_the_title($current)) {
+            echo "<a href='".$url."'><i class='fa fa-home'></i></a>&nbsp;»&nbsp;
+            <a href='".$url."/".$grandparenturl."/".$parenturl."'>". $parenturl ."</a>&nbsp;»&nbsp;"
+            .get_the_slug($current);
+        }
+        else if($root_parent = get_the_title($child) == $root_parent = get_the_title($child)) {
+            echo "<a href='".$url."'><i class='fa fa-home'></i></a>&nbsp;»&nbsp;"
+            .get_the_slug($current);;
+        }
+        else {
+            echo get_the_slug($child);
+        }
     ?>
 </div>
