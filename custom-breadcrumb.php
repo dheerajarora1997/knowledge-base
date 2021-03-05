b3a5cdd2fe4f8881010a822df54407891f260231
Dheeraj Arora <dheeraj.arora1997@yahoo.com>
2021-03-05 12:43:31 +0530

    Create custom-breadcrumb.php

---
+++ custom-breadcrumb.php
@@ -0,0 +1,56 @@
+//Code for function.php 
+
+function the_slug($id = null)
+{
+    echo apply_filters('the_slug', get_the_slug($id));
+}
+
+function get_the_slug($id = null)
+{
+    if (empty($id)) {
+        global $post;
+        if (empty($post)) {
+            return '';
+        }
+
+        $id = $post->ID;
+    }
+    $slug = basename(get_permalink($id));
+    return $slug;
+}
+
+//page code
+
+
+<?php
+  $current = $post->ID;
+  $parent = $post->post_parent;
+  $grandparent_get = get_post($parent);
+  $grandparent = $grandparent_get->post_parent;
+?>
+
+<div class="npf-breadcrumb">
+    <?php
+    $url = home_url();
+    $grandparenturl = get_the_slug($grandparent);
+    $parenturl = get_the_slug($parent);
+    if ($root_parent = get_the_title($grandparent) !== $root_parent = get_the_title($child)){
+        echo "<a href='".$url."'><i class='fa fa-home'></i></a>&nbsp; »
+        <a href='".$url."/".$grandparenturl."'>". $grandparenturl ."</a> »
+        <a href='".$url."/".$grandparenturl."/".$parenturl."'>". $parenturl ."</a> »"
+        .get_the_slug($current);
+    }
+    else if($root_parent = get_the_title($parent) !== $root_parent = get_the_title($current)) {
+        echo "<a href='".$url."'><i class='fa fa-home'></i></a>&nbsp; »
+        <a href='".$url."/".$grandparenturl."/".$parenturl."'>". $parenturl ."</a> »"
+        .get_the_slug($current);
+    }
+    else if($root_parent = get_the_title($child) == $root_parent = get_the_title($child)) {
+        echo "<a href='".$url."'><i class='fa fa-home'></i></a>&nbsp; » "
+        .get_the_slug($current);;
+    }
+    else {
+        echo get_the_slug($child);
+    }
+    ?>
+</div>
