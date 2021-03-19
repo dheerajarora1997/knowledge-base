3ea9b94ebcd901dc990fa8fc1cd2ee1bac58e6c5
Dheeraj Arora <dheeraj.arora1997@yahoo.com>
2021-03-19 17:48:02 +0530

    Update custom-breadcrumb.php

--- custom-breadcrumb.php
+++ custom-breadcrumb.php
@@ -1,56 +1,89 @@
-//Code for function.php 
-
-function the_slug($id = null)
-{
-    echo apply_filters('the_slug', get_the_slug($id));
-}
-
-function get_the_slug($id = null)
-{
-    if (empty($id)) {
-        global $post;
-        if (empty($post)) {
-            return '';
-        }
-
-        $id = $post->ID;
-    }
-    $slug = basename(get_permalink($id));
-    return $slug;
-}
-
-//page code
-
-
 <?php
-  $current = $post->ID;
-  $parent = $post->post_parent;
-  $grandparent_get = get_post($parent);
-  $grandparent = $grandparent_get->post_parent;
+    $current = $post->ID;
+    $parent = $post->post_parent;
+    $grandparent_get = get_post($parent);
+    $grandparent = $grandparent_get->post_parent;
 ?>
+<?php
 
+    function clean($string) {
+        // $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
+
+        return preg_replace('/[-]/', ' ', $string); // Removes special chars.
+    }
+
+?>
 <div class="npf-breadcrumb">
     <?php
-        $url = home_url();
-        $grandparenturl = get_the_slug($grandparent);
-        $parenturl = get_the_slug($parent);
-        if ($root_parent = get_the_title($grandparent) !== $root_parent = get_the_title($child)){
-            echo "<a href='".$url."'><i class='fa fa-home'></i></a>&nbsp;»&nbsp;
-            <a href='".$url."/".$grandparenturl."'>". $grandparenturl ."</a>&nbsp;»&nbsp;
-            <a href='".$url."/".$grandparenturl."/".$parenturl."'>". $parenturl ."</a>&nbsp;»&nbsp;"
-            .get_the_slug($current);
-        }
-        else if($root_parent = get_the_title($parent) !== $root_parent = get_the_title($current)) {
-            echo "<a href='".$url."'><i class='fa fa-home'></i></a>&nbsp;»&nbsp;
-            <a href='".$url."/".$grandparenturl."/".$parenturl."'>". $parenturl ."</a>&nbsp;»&nbsp;"
-            .get_the_slug($current);
-        }
-        else if($root_parent = get_the_title($child) == $root_parent = get_the_title($child)) {
-            echo "<a href='".$url."'><i class='fa fa-home'></i></a>&nbsp;»&nbsp;"
-            .get_the_slug($current);;
-        }
-        else {
-            echo get_the_slug($child);
-        }
+    $url = home_url();
+    $grandparenturl = get_the_slug($grandparent);
+    $parenturl = get_the_slug($parent);
+    if ($root_parent = get_the_title($grandparent) !== $root_parent = get_the_title($child)){
+        echo "
+            <ol itemscope itemtype='https://schema.org/BreadcrumbList'>
+                <li itemprop='itemListElement' itemscope itemtype='https://schema.org/ListItem'>
+                    <a itemprop='item' href='".$url."'>
+                        <span itemprop='name'><i class='fa fa-home'></i><i class='sr-only'>Home</i></span>
+                    </a>
+                    <meta itemprop='position' content='1' />
+                </li>&nbsp;»&nbsp;
+                <li itemprop='itemListElement' itemscope itemtype='https://schema.org/ListItem'>
+                    <a  itemprop='item' href='".$url."/".$grandparenturl."'>
+                        <span itemprop='name'>". clean($grandparenturl) ."</span>
+                    </a>
+                    <meta itemprop='position' content='2' />
+                </li>&nbsp;»&nbsp;
+                <li itemprop='itemListElement' itemscope itemtype='https://schema.org/ListItem'>
+                    <a itemprop='item' href='".$url."/".$grandparenturl."/".$parenturl."'>
+                        <span itemprop='name'>". clean($parenturl) ."</span>
+                    </a>
+                    <meta itemprop='position' content='3' />
+                </li>&nbsp;»&nbsp;
+                <li itemprop='itemListElement' itemscope itemtype='https://schema.org/ListItem'>
+                    <span itemprop='name'>". clean(get_the_slug($current)) ."</span>
+                    <meta itemprop='position' content='4' />
+                </li>
+            </ol>"
+        ;}
+    else if($root_parent = get_the_title($parent) !== $root_parent = get_the_title($current)) {
+        echo "
+            <ol itemscope itemtype='https://schema.org/BreadcrumbList'>
+                <li itemprop='itemListElement' itemscope itemtype='https://schema.org/ListItem'>
+                    <a itemprop='item' href='".$url."'>
+                        <span itemprop='name'><i class='fa fa-home'></i><i class='sr-only'>Home</i></span>
+                        <meta itemprop='position' content='1' />
+                    </a>
+                </li>&nbsp;»&nbsp;
+                <li itemprop='itemListElement' itemscope itemtype='https://schema.org/ListItem'>
+                    <a  itemprop='item'
+                        href='".$url."/".$grandparenturl."/".$parenturl."'>
+                        <span itemprop='name'>". clean($parenturl) ."</span>
+                    </a>
+                    <meta itemprop='position' content='2' />
+                </li>&nbsp;»&nbsp;
+                <li itemprop='itemListElement' itemscope itemtype='https://schema.org/ListItem'>
+                    <span itemprop='name'>". clean(get_the_slug($current)) ."</span>
+                    <meta itemprop='position' content='3' />
+                </li>
+            </ol>"
+        ;}
+    else if($root_parent = get_the_title($current) == $root_parent = get_the_title($child)) {
+        echo "
+            <ol itemscope itemtype='https://schema.org/BreadcrumbList'>
+                <li itemprop='itemListElement' itemscope itemtype='https://schema.org/ListItem'>
+                    <a itemprop='item' href='".$url."'>
+                        <span itemprop='name'><i class='fa fa-home'></i><i class='sr-only'>Home</i></span>
+                    </a>
+                    <meta itemprop='position' content='1' />
+                </li>&nbsp;»&nbsp;
+                <li itemprop='itemListElement' itemscope itemtype='https://schema.org/ListItem'>
+                    <span itemprop='name'>". clean(get_the_slug($current)) ."</span>
+                    <meta itemprop='position' content='2' />
+                </li>
+            </ol>"
+    ;}
+    else {
+        echo get_the_slug($child);
+    }
     ?>
 </div>
